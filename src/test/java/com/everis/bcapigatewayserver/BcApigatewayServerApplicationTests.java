331208a31bb1c5d5e655d278387d91e76aaf7222
DAlembert721 <dalembert.dmu@gmail.com>
2021-08-10 23:06:51 -0500

    feat: Add api gateway

---
+++ src/test/java/com/everis/bcapigatewayserver/BcApigatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.everis.bcapigatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BcApigatewayServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
